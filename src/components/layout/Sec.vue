e6c650e8ea291336cf0252390c51967a8882411b
Gabe Dunn <gabe.dunn@shaw.ca>
2020-07-17 20:11:08 -0600

    add some elements

---
+++ src/components/layout/Sec.vue
@@ -0,0 +1,17 @@
+<template>
+  <section :id="id" class="my-6 md:my-12 lg:my-18">
+    <slot />
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'Sec',
+    props: {
+      id: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
---
+++ src/components/titles/ElementTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3
+    class="font-medium text-gray-900 text-base sm:text-lg leading-5 sm:leading-7"
+  >
+    <slot />
+  </h3>
+</template>
+
+<script>
+  // TODO: potentially add subtitle option
+
+  export default {
+    name: 'ElementTitle'
+  }
+</script>
